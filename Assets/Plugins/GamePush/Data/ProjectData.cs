8a881c6543612f8a54ccf0a7747797db6384a23f
JesusFilth <Adrian_McArrow@mail.ru>
2024-12-03 13:22:50 +0300

    Change to WebGl

--- Assets/Plugins/GamePush/Data/ProjectData.cs
+++ Assets/Plugins/GamePush/Data/ProjectData.cs
@@ -3,10 +3,10 @@ namespace GamePush.Data
     public static class ProjectData
     {
         public static string SDK_VERSION = "1.7.0";
-        public static string ID = "1";
-        public static string TOKEN = "token";
-        public static bool GAMEREADY_AUTOCALL = false;
-        public static bool SHOW_STICKY_ON_START = false;
+        public static string ID = "18788";
+        public static string TOKEN = "S9FqzvjqFxgXO92jrHb0Kp3vkHwwiDKQ";
+        public static bool GAMEREADY_AUTOCALL = true;
+        public static bool SHOW_STICKY_ON_START = true;
         public static bool WAIT_PLAGIN_READY = false;
     }
 }
