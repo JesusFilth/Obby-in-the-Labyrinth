5e63227199c52a11454ef586734da3d6f77b65ba
JesusFilth <Adrian_McArrow@mail.ru>
2024-12-06 12:40:11 +0300

    Change scripts folder

--- Assets/_Source_/Scripts/Core/Game FSM/LoadDataState.cs
+++ Assets/_Source_/Scripts/Core/Game FSM/LoadDataState.cs
@@ -21,19 +21,4 @@ public class LoadDataState : IGameState
     {
         _stateMashine.EnterIn<LoadGameSceneState>();
     }
-
-    private UserModel GetDefaultUser()
-    {
-        UserModel userModel = new UserModel()
-        {
-            Name = "Player",
-            Gold = 100,
-            Score = 0,
-        };
-
-        userModel.Levels = new LevelModel[1];
-        userModel.Levels[0] = new LevelModel() { Number = 1, Stars = 0 };
-
-        return userModel;
-    }
 }
